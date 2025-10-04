ebf3f37acc403bbff8663dbe07c297d3d770a78d
webdizzy777 <carissa.mcmillian@gmail.com>
2025-10-04 13:25:44 -0500

    Initial commit

---
+++ miles-and-smiles/src/main/java/com/example/miles_and_smiles/MilesAndSmilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.miles_and_smiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilesAndSmilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilesAndSmilesApplication.class, args);
+	}
+
+}
